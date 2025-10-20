c3cced4c60a865c12f49daa6e8c96d601fd4cdc0
shahxvi <2025171523@student.uitm.edu.my>
2025-10-21 01:25:54 +0800

    Create KTMB practice

---
+++ sem2/CSC186/W3/KTMB.java
@@ -0,0 +1,111 @@
+import java.util.Scanner;
+
+public class KTMB {
+        public static void main(String[] args) {
+                Scanner keyboard = new Scanner(System.in);
+                char repeat;
+
+                do {
+                        // Clear Screen
+                        System.out.print("\033[H\033[2J");
+                        System.out.flush();
+
+                        String passengerID;
+                        char packageCode;
+                        int adultQuantity = 0;
+                        int childQuantity = 0;
+
+                        double ticketPrice;
+                        final double ADULT_GOLD_PRICE = 59.00;
+                        final double CHILD_GOLD_PRICE = 34.00;
+                        final double ADULT_PLATINUM_PRICE = 79.00;
+                        final double CHILD_PLATINUM_PRICE = 44.00;
+                        double total;
+
+                        boolean addOn;
+                        final double MEAL_COMBO_PRICE = 7.00;
+                        int mealComboQuantity = 0;
+                        double mealComboCharge = 0.00;
+
+                        // Get Pessenger ID
+                        System.out.print("Please enter Passenger ID: ");
+                        passengerID = keyboard.nextLine();
+
+                        System.out.println(); // line seperator
+
+                        // Table of Prices
+                        System.out.println("| Type of Package  | Ticket Category | Ticket Price (RM) |");
+                        System.out.println("| ---------------- | --------------- | ----------------- |");
+                        System.out.printf("| ETS Gold (G)     | Adult           | %.2f             |\n", ADULT_GOLD_PRICE);
+                        System.out.printf("| ETS Gold (G)     | Child           | %.2f             |\n", CHILD_GOLD_PRICE);
+                        System.out.printf("| ETS Platinum (P) | Adult           | %.2f             |\n", ADULT_PLATINUM_PRICE);
+                        System.out.printf("| ETS Platinum (P) | Child           | %.2f             |\n", CHILD_PLATINUM_PRICE);
+
+                        // Get Package Code
+                        System.out.print("Please enter package code (G / P): ");
+                        packageCode = Character.toUpperCase(keyboard.next().charAt(0));
+
+                        // For invalid Package Code
+                        while (!(packageCode == 'G' || packageCode == 'P')) {
+                                System.out.println("Invalid Package Code");
+                                System.out.print("Please enter package code (G / P): ");
+                                packageCode = Character.toUpperCase(keyboard.next().charAt(0));
+                        }
+
+                        System.out.println(); // line seperator
+
+                        System.out.print("Please enter quantity of adults: ");
+                        adultQuantity = keyboard.nextInt();
+
+                        System.out.println(); // line seperator
+
+                        System.out.print("Please enter quantity of children: ");
+                        childQuantity = keyboard.nextInt();
+
+                        // Calculation for ticket price
+                        if (packageCode == 'G') {
+                                ticketPrice = (adultQuantity * ADULT_GOLD_PRICE) + (childQuantity * CHILD_GOLD_PRICE);
+                        } else {
+                                ticketPrice = (adultQuantity * ADULT_PLATINUM_PRICE) + (childQuantity * CHILD_PLATINUM_PRICE);
+                        }
+
+                        System.out.println(); // line seperator
+
+                        // Confirmation for meal combo
+                        System.out.print("Do you want add on meal combo? (Y/N): ");
+                        addOn = keyboard.next().equalsIgnoreCase("Y");
+
+                        // Get quantity and calculate charge
+                        if (addOn) {
+                                System.out.print("Enter quantity of meal combo: ");
+                                mealComboQuantity = keyboard.nextInt();
+
+                                mealComboCharge = mealComboQuantity * MEAL_COMBO_PRICE;
+                        }
+
+                        // Calculate final @ total charge
+                        total = ticketPrice + mealComboCharge;
+
+                        System.out.println(); // line seperator
+
+                        // Order Summary
+                        System.out.println("ETS TICKET INFORMATION - Butterworth to Kuala Lumpur");
+                        System.out.printf("PASSENGER ID:\t\t%s\n", passengerID);
+                        System.out.printf("Number of Adults:\t%d\n", adultQuantity);
+                        System.out.printf("Number of Childrens:\t%d\n", childQuantity);
+                        System.out.printf("Ticket Price:\t\tRM%,.2f\n", ticketPrice);
+                        System.out.printf("Meal Combo Price:\tRM%,.2f\n", mealComboCharge);
+                        System.out.printf("Net Ticket Price:\tRM%,.2f\n", total);
+
+                        System.out.println(); // line seperator
+
+                        System.out.print("Repeat for a different Customer? (Y/N): ");
+                        repeat = Character.toUpperCase(keyboard.next().charAt(0));
+                        keyboard.nextLine(); // consume the leftover \n
+
+                        System.out.println(); // line seperator
+                } while (!(repeat == 'N'));
+
+                keyboard.close();
+        }
+}
