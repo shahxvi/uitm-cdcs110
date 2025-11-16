075193a335b2a387c4f3c4e55c7ad55362d6eb2f
shahxvi <2025171523@student.uitm.edu.my>
2025-11-16 10:09:22 +0800

    Add license

--- sem2/CSC186/W06/ClothApp.java
+++ sem2/CSC186/W06/ClothApp.java
@@ -1,62 +1,79 @@
+/* MIT License
+ * Copyright (c) <2025> <Shah> <2025171523@student.uitm.edu.my>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in shall
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE. */
+
 import java.util.Scanner;
 
 public class ClothApp {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        int numberOfCustomers = 3;
+        int numberOfCustomers = 2;
         Cloth[] cloths = new Cloth[numberOfCustomers];
 
-        for (int i = 0; i < cloths.length; i++) {
-            cloths[i] = new Cloth();
+        double totalPayment = 0;
+        int customersWithMoreThan10Meters = 0;
+
+        for (Cloth c : cloths) {
+            c = new Cloth();
 
             System.out.print("\nEnter your name: ");
-            cloths[i].setName(keyboard.nextLine());
+            c.setName(keyboard.nextLine());
 
             System.out.print("Enter cloth price: ");
-            cloths[i].setPrice(keyboard.nextDouble());
+            c.setPrice(keyboard.nextDouble());
             keyboard.nextLine();
 
             System.out.print("Enter cloth length: ");
-            cloths[i].setLength(keyboard.nextDouble());
+            c.setLength(keyboard.nextDouble());
             keyboard.nextLine();
-
-            System.out.print(cloths[i]);
         }
+
+        for (Cloth c : cloths)
+            System.out.print(c);
+
+        System.out.println("\nList of Customers: ");
+        for (Cloth c : cloths)
+            System.out.println(c);
+
         keyboard.close();
 
-        System.out.printf("\nTotal payment from all customers: RM%,.2f", totalPayment(cloths));
-        System.out.println("\nNumber of customers with more than 10 meters of cloth: " + customersWithMoreThan10Meters(cloths));
-        System.out.println("\nCustomer with the highest payment:" + highestPayment(cloths));
-    }
+        for (Cloth c : cloths) {
+            totalPayment += c.calcPayment();
+        }
+        System.out.printf("\nTotal payment from all customers: RM%,.2f", totalPayment);
 
-    public static int customersWithMoreThan10Meters(Cloth[] cloths) {
-        int customersWithMoreThan10Meters = 0;
         for (Cloth c : cloths) {
             if (c.getLength() > 10.00) {
                 customersWithMoreThan10Meters++;
             }
         }
-        return customersWithMoreThan10Meters;
-    }
+        System.out.println("\nNumber of customers with more than 10 meters of cloth: " + customersWithMoreThan10Meters);
 
-    public static double totalPayment(Cloth[] cloths) {
-        double totalPayment = 0;
-        for (Cloth c : cloths) {
-            totalPayment += c.calcPayment();
-        }
-        return totalPayment;
-    }
-
-    public static Cloth highestPayment(Cloth[] cloths) {
         double highestPayment = -9999999.99;
-        int i = 0;
+        int index = 0;
         for (Cloth c : cloths) {
             if (c.calcPayment() > highestPayment) {
                 highestPayment = c.calcPayment();
             } else {
-                i++;
+                index++;
             }
         }
-        return cloths[i];
+        System.out.println("\nCustomer with the highest payment:" + cloths[index]);
     }
 }
